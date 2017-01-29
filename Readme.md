861093a6f72231a3400a8b1f929e049d5c672c66
George Michokostas <michokostas@gmail.com>
2017-01-29 17:24:15 +0000

    Very basic step counter implementation

---
+++ Readme.md
@@ -0,0 +1,3 @@
+### Step Counter
+
+- a very basic implemtation of a step counter using CoreMotion framework
--- StepCounter/ViewController.swift
+++ StepCounter/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stepsCounterLabel: UILabel!
+    @IBOutlet weak var controlButton: UIButton!
+    @IBOutlet weak var lastSessionCounterLabel: UILabel!
+
+    let pedometer   = CMPedometer()
+    let currentDate = Date()
+    let defaults    = UserDefaults.standard
+    let alert = UIAlertController(title: "Error",
+                                       message: "Pedometer is not available",
+                                       preferredStyle: .alert)
+    let defaultAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:nil)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.stepsCounterLabel.text = "0"
+        self.lastSessionCounterLabel.text = self.defaults.string(forKey: "userSteps")
+        self.controlButton.setTitle("Start", for: .normal)
+        alert.addAction(defaultAction)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stepsButton (sender: UIButton){
+        switch sender.currentTitle! {
+        case "Start":
+            self.controlButton.setTitle("Stop", for: .normal)
+            self.startCounting()
+        case "Stop":
+            self.controlButton.setTitle("Start", for: .normal)
+            self.stopCounting()
+        default : break    
+        }
+    }
 
+    private func startCounting() {
+        if CMPedometer.isStepCountingAvailable() {
+            self.pedometer.startUpdates(from: Date()) { (data: CMPedometerData?, error) -> Void in
+                DispatchQueue.main.async(execute: { () -> Void in
+                    if(error == nil) {
+                        print("\(data!.numberOfSteps)")
+
+                        self.controlButton.setTitle("Stop", for: .normal)
+                        self.defaults.set("\(data!.numberOfSteps)", forKey: "userSteps")
+                        self.stepsCounterLabel.text = "\(data!.numberOfSteps)"
+                    } else {
+                        self.disableButton(button: self.controlButton)
+                        self.present(self.alert, animated: false, completion: nil)
+                    }
+                })
+            }
+        } else {
+            print("Pedometer is not available")
+            self.disableButton(button: self.controlButton)
+            present(alert, animated: false, completion: nil)
+        }
+    }
+
+    private func stopCounting() {
+        self.lastSessionCounterLabel.text = self.defaults.string(forKey: "userSteps")
+        self.pedometer.stopUpdates()
+    }
+
+    private func disableButton(button: UIButton) {
+        button.setTitle("Start", for: .normal)
+        button.isUserInteractionEnabled = false
+    }
 }
 
